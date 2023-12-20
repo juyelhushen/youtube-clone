e87afbe8698a01d8f98212696099d21369b8b4b9
Juyel <juyelhushen@gmail.com>
2023-12-20 16:41:24 +0530

    Cloud Server aplication has been included

---
+++ yc-config-server/src/test/java/com/jskool/configserver/YcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jskool.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
